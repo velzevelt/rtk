ad61e5e974eb7d016f2854cdd7ca582e7c426501
velz <novaraptor13@gmail.com>
2022-11-06 16:57:49 +0300

    new prs

--- pr_6/pr_6.php
+++ pr_6/pr_6.php
@@ -1,3 +1,52 @@
 <?php 
 
+# Задача 1. Написать программу, змейку, которая съедает линию
+# Результат работы класса – файл в котором на первой строчке исходная линия, а 
+# далее в каждой строке пошаговое «съедание» линии змейкой
+# Пример реализации
+# 1)>-------------------
+# 2)*>------------------
+# 3)**********>---------
+# 4)****************>---
+
+class Snake
+{
+    private $_base_line = "";
+    private $_filename = "";
+
+    function __construct($filename, $out_path = ".o")
+    {
+        // if( $file = fopen($filename, 'r') )
+        // {
+        //     $_base_line = fread($file, filesize($filename));
+        //     fclose($file);
+        // }
+        $this->_base_line = file_get_contents($filename);
+        $this->_filename = $filename . $out_path;
+    }
+    
+    function main(): void
+    {   
+        if( $file = fopen( $this->_filename, "w+") )
+        {
+            $line = $this->_base_line;
+
+            for($head_pos = 0, $j = strlen( $line ); $head_pos < $j; $head_pos++)
+            {
+               $line[$head_pos] = '>';
+               $line = substr_replace($line, str_repeat('*', $head_pos), 0, $head_pos);
+               fwrite($file, $line . "\n"); 
+            }
+
+            fclose($file);
+        }
+    }
+}
+
+
+$snake = new Snake("snake.txt");
+$snake->main();
+
+
+
 ?>
--- pr_7/pr_7.php
+++ pr_7/pr_7.php
@@ -1,3 +1,121 @@
+<pre>
 <?php 
+const ROUNDS = 3;
+
+const MAX_UNITS_IN_ARMY = 25;
+const MAX_UNIT_HEALTH = 100;
+const MAX_UNIT_DAMAGE = 45;
+const MIN_UNIT_DAMAGE = 5;
+
+class Game
+{
+    public $white_army;
+    public $black_army;
+    function __construct($white_army, $black_army)
+    {
+        $this->white_army = $white_army;
+        $this->black_army = $black_army;
+
+        $this->white_army->enemy_units = $this->black_army->units;
+        $this->black_army->enemy_units = $this->white_army->units;
+    }
+
+    function main(): void
+    {
+        $fate = mt_rand(0, 1);
+        $first_player = $fate == 1 ? $this->white_army : $this->black_army;
+        $second_player = $fate == 1 ? $this->black_army : $this->white_army;
+
+        $current_player = $first_player;
+
+        while( $current_player->can_move() )
+        {
+            $current_player->make_move();
+            $current_player = $current_player == $first_player ? $second_player : $first_player;
+        }
+
+        var_dump($current_player);
+
+    }
+}
+
+class Army
+{
+    public $units = [];
+    public $enemy_units = [];
+    public $name = "";
+    public $kills = 0;
+
+    function __construct($name = '')
+    {
+        for($i = 0; $i < MAX_UNITS_IN_ARMY; $i++)
+        {
+            array_push( $this->units, new Unit() );
+        }
+        $this->name = $name;
+    }
+
+
+    function make_move()
+    {
+        foreach($this->units as $unit)
+        {
+            $target_id = array_rand( $this->enemy_units );
+            $target = $this->enemy_units[$target_id];
+            $unit -> attack($target);
+
+            if( !($target->is_valid()) ) # Enemy killed
+            {
+                unset( $this->enemy_units[$target_id] );
+                $this->enemy_units = array_values($this->enemy_units);
+                $this->kills++;
+            }
+        }
+    }
+    function can_move(): bool
+    {
+        return ($this->kills < 7);
+    }
+}
+
+class Unit
+{
+    public $health;
+    public $damage;
+    public $active = true;
+    public $destroyed = false;
+
+    function __construct()
+    {   
+        $this -> health = MAX_UNIT_HEALTH;
+        $this -> damage = random_int(MIN_UNIT_DAMAGE, MAX_UNIT_DAMAGE);
+    }
+
+    function take_damage($damage)
+    {
+        $this->health -= $damage;
+
+        if($this->health <= 0)
+        {
+            $this->active = false;
+            $this->destroyed = true;
+        }
+
+    }
+
+    function attack(Unit $target)
+    {
+        $target->take_damage( $this->damage );
+    }
+
+    function is_valid(): bool
+    {
+        return ($this->active and !($this->destroyed) );
+    }
+}
+
+$game = new Game( new Army('white'), new Army('black') );
+
+$game->main();
 
 ?>
