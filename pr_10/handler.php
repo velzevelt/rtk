e4b67dac0a209c965c1b1105be64d9882038df2e
Class_Student <Class_Student@rtk.local>
2022-11-16 14:31:03 +0300

    pr upd

--- pr_10/handler.php
+++ pr_10/handler.php
@@ -1,15 +1,28 @@
 <?php
 require_once 'vendor/autoload.php';
+use Imagine\Imagick\Imagine as Imagine;
+use Imagine\Image\Point as Point;
+use Imagine\Image\Box as Box;
 
-const SAVE_DIR = 'download/';
-const WATERMARK = 'assets/watermark.png';
+const SAVE_DIR = "out/";
 
 if ( isset($_POST['send']) )
 {
-    if (isset($_FILES['userfile']) and is_uploaded_file($_FILES['userfile']['tmp_name'])) {
-        $image = $_FILES['userfile'];
+    if (isset( $_FILES['user_image']) and isset($_FILES['user_watermark']) ) {
+        $image = $_FILES['user_image'];
+        $watermark = $_FILES['user_watermark'];
     }
+
 }
+
+
+$imagine = new Imagine();
+$imagine -> open('assets/image.png')
+         -> save('out/i.png');
+
+
+// $image_wm = new ImageWatermark('assets/image.png', 'assets/watermark.png');
+// $image_wm->show_image();
 
 
 class ImageWatermark
@@ -21,32 +34,40 @@ class ImageWatermark
     
     function __construct( $image, $watermark, $new_image_size = [400, 250], $out_path = SAVE_DIR, )
     {   
-        $this->imagine = new Imagine\Gd\Imagine();
-        $this->image = $image;
-        $this->watermark = $watermark;
+        $this->imagine = new Imagine();
         if(isset($image) and isset($watermark))
         {
+            $this->image = $this->imagine->open($image);
+            $this->watermark = $this->imagine->open($watermark);
+            
             $this->new_image_size['x'] = $new_image_size[0];
             $this->new_image_size['y'] = $new_image_size[1];
 
-            $this->main();
+            
+            $this->apply_watermark();
         }
     }
-    private function main(): void
+    private function apply_watermark(): void
     {
+        $image_size = $this->image->getSize();
+        $this->watermark->resize(new Box($image_size->getWidth() / 2, $image_size->getHeight() / 2));
+        $w_size = $this->watermark->getSize();
         
+        // $bottom_right = new Point($image_size->getWidth() - $w_size->getWidth(), $image_size->getHeight() - $w_size->getHeight());
+        $center = new Point($image_size->getWidth() / 2, $image_size->getHeight() / 2);
+
+        $this->image->paste($this->watermark, $center);
+
+        $this->image->resize( new Box($this->new_image_size['x'], $this->new_image_size['y']) );
+        $this->image->save('i.png');
+    }
+
+    public function show_image(): void
+    {
+        $this->image->show("png");
     }
 }
 
 
-
-// $watermark = $imagine->open('/my/watermark.png');
-// $image     = $imagine->open('/path/to/image.jpg');
-// $size      = $image->getSize();
-// $wSize     = $watermark->getSize();
-
-// $bottomRight = new Imagine\Image\Point($size->getWidth() - $wSize->getWidth(), $size->getHeight() - $wSize->getHeight());
-
-// $image->paste($watermark, $bottomRight);
 
 ?>
