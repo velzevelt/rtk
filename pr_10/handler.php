1296babb834000f3fe391d7a7bff4d47bcd8ffa4
Class_Student <Class_Student@rtk.local>
2022-11-22 09:10:14 +0300

    imagick problem need fix

--- pr_10/handler.php
+++ pr_10/handler.php
@@ -4,6 +4,8 @@ use Imagine\Imagick\Imagine as Imagine;
 use Imagine\Image\Point as Point;
 use Imagine\Image\Box as Box;
 
+
+
 const SAVE_DIR = "out/";
 
 if ( isset($_POST['send']) )
@@ -11,6 +13,8 @@ if ( isset($_POST['send']) )
     if (isset( $_FILES['user_image']) and isset($_FILES['user_watermark']) ) {
         $image = $_FILES['user_image'];
         $watermark = $_FILES['user_watermark'];
+
+        var_dump($image);
     }
 
 }
@@ -21,7 +25,7 @@ $imagine -> open('assets/image.png')
          -> save('out/i.png');
 
 
-// $image_wm = new ImageWatermark('assets/image.png', 'assets/watermark.png');
+// $image_wm = new ImageWatermark('assets/image.jpg', 'assets/watermark.png');
 // $image_wm->show_image();
 
 
@@ -32,7 +36,7 @@ class ImageWatermark
     public $watermark = null;
     public $new_image_size = ['x' => 0, 'y' => 0];
     
-    function __construct( $image, $watermark, $new_image_size = [400, 250], $out_path = SAVE_DIR, )
+    function __construct( $image, $watermark, $new_image_size = [400, 250], $out_path = SAVE_DIR )
     {   
         $this->imagine = new Imagine();
         if(isset($image) and isset($watermark))
@@ -51,7 +55,6 @@ class ImageWatermark
     {
         $image_size = $this->image->getSize();
         $this->watermark->resize(new Box($image_size->getWidth() / 2, $image_size->getHeight() / 2));
-        $w_size = $this->watermark->getSize();
         
         // $bottom_right = new Point($image_size->getWidth() - $w_size->getWidth(), $image_size->getHeight() - $w_size->getHeight());
         $center = new Point($image_size->getWidth() / 2, $image_size->getHeight() / 2);
@@ -59,12 +62,12 @@ class ImageWatermark
         $this->image->paste($this->watermark, $center);
 
         $this->image->resize( new Box($this->new_image_size['x'], $this->new_image_size['y']) );
-        $this->image->save('i.png');
+        $this->image->save(SAVE_DIR . 'image.jpg');
     }
 
     public function show_image(): void
     {
-        $this->image->show("png");
+        $this->image->show("jpg");
     }
 }
 
