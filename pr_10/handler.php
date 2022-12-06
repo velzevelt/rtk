bed7f0528a0abde72561e21a8fe18be46ad8e9b6
velzevelt <velzcontact@yandex.ru>
2022-12-06 14:37:44 +0300

    upd

--- pr_10/handler.php
+++ pr_10/handler.php
@@ -20,6 +20,11 @@ if ( isset($_POST['send']) )
 } else {
     
     ### DEBUG ###
+
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
+
     $imagine = new Imagine();
     $image = new ImageWatermark('assets/cat.jpg', 'assets/watermark.png');
     $image->show_image();
