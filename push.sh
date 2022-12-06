31b6bb548e79f9e211132809982c836923ce7a5d
Class_Student <Class_Student@rtk.local>
2022-12-06 09:51:19 +0300

    upd

--- push.sh
+++ push.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-STANDART_MESSAGE="upd";
+STANDART_MESSAGE="upd"
 
 
 if [ -n "$1" ]
 then
-    git add .;
-    git commit -m $1;
-    git push;
+    git add .
+    git commit -m $1
+    git push
 
 else
-    echo "Не было задано сообщение коммита";
-    echo "Использовано стандартное сообщение $STANDART_MESSAGE";
-    git add .;
-    git commit -m $STANDART_MESSAGE;
-    git push;
+    echo "Не было задано сообщение коммита"
+    echo "Использовано стандартное сообщение $STANDART_MESSAGE"
+    git add .
+    git commit -m $STANDART_MESSAGE
+    git push
 fi
