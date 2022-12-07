dbf59c3a3e971a7f672c8f24725692f3e49760f7
Class_Student <Class_Student@rtk.local>
2022-12-07 10:40:35 +0300

    upd

--- pr_6/pr_6.php
+++ pr_6/pr_6.php
@@ -11,19 +11,19 @@
 
 class Snake
 {
-    protected $_base_line = "";
-    private $_filename = "";
+    public $base_line = "";
+    public $filename = "";
 
-    function __construct($filename, $out_path = ".o")
+    function __construct($_filename, $_out_path = ".o")
     {
-        $this->_base_line = file_get_contents($filename);
-        $this->_filename = $filename . $out_path;
+        $this->base_line = file_get_contents($_filename);
+        $this->filename = $_filename . $_out_path;
     }
 
     function main(): void
     {
-        if ($file = fopen($this->_filename, "w+")) {
-            $line = $this->_base_line;
+        if ($file = fopen($this->filename, "w+")) {
+            $line = $this->base_line;
 
             for ($head_pos = 0, $j = strlen($line); $head_pos < $j; $head_pos++) {
                 $line[$head_pos] = '>';
@@ -40,10 +40,55 @@ class Snake
 // $snake = new Snake("snake.txt");
 // $snake->main();
 
-// $snake2D = new Snake2D('snake2D.txt');
-// $snake2D->main();
+$snake2D = new Snake2D('snake2D.txt');
+$snake2D->main();
 
 class Snake2D
 {
-    
+    public $border_char;
+    public $tick;
+
+    const BLOCKED = 0; # Нельзя ходить, клетка занята (граница или змея)
+    const FREE = 1; # 1 Поле свободно, змея может здесь быть
+    public $cell = ['line_id' => 0, 'position', 'state']; # [x, y, (BLOCKED || FREE) ]. Клетка поля
+    public $space = []; # array of cells
+
+    /**
+     * Summary of __construct
+     * @param mixed $_filename
+     * @param mixed $_border_char Символ конца игрового поля
+     * @param mixed $_tick Время одного хода (в микросекундах)
+     */
+    function __construct($_filename, $_border_char = '|', $_tick = 250000) {
+
+        $content = str_split(file_get_contents($_filename));
+
+        $this->border_char = $_border_char;
+        $this->tick = $_tick;
+
+        $space = $this->space;
+        $cell = $this->cell;
+        $x = 0;
+        # Формируем игровое поле
+        foreach($content as $key => $char) {
+            if($char == "\n") { # Начало новой строки
+                $x++;
+                $cell['line_id'] = $x;
+            } else {
+                $cell['position'] = $key;
+                
+                if ($char == $_border_char) {
+                    $cell['state'] = Snake2D::BLOCKED;
+                } else {
+                    $cell['state'] = Snake2D::FREE;
+                }
+            }
+            $space[$key] = $cell;
+        }
+        $this->space = $space;
+    }
+
+    public function main(): void {
+        var_dump($this->space);
+    }
 }
