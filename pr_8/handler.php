a43c43ec02463e2bd4594fe78240e25023c7027e
Class_Student <Class_Student@rtk.local>
2022-12-21 13:57:30 +0300

    upd

--- pr_8/handler.php
+++ pr_8/handler.php
@@ -13,9 +13,10 @@ class Form
 
     public $file;
 
+
     const SAVE_DIR = "download/";
 
-    function __construct()
+    public function __construct()
     {
         if (isset($_POST['send'])) {
             $this->name = trim(strip_tags($_POST['name']));
@@ -28,23 +29,24 @@ class Form
             $this->antivirus_comment = trim(strip_tags($_POST['antivirus_comment']));
             $this->antivirus_rating = strip_tags($_POST['antivirus_rating']);
 
+
             if (isset($_FILES['userfile']) and is_uploaded_file($_FILES['userfile']['tmp_name'])) {
                 $this->file = $_FILES['userfile'];
             }
         }
     }
 
-    function save_file(): string
+    public function save_file(): string
     {
         $res = false;
         $file = $this->file;
         $tmp_file = $file['tmp_name'];
         if (isset($file)) {
-            $to_file =  self::SAVE_DIR . basename($file['name']);
+            $to_file = self::SAVE_DIR . $this->get_file_name();
             if (move_uploaded_file($tmp_file, $to_file)) {
                 $res = "Файл успешно загружен";
             } else {
-                $res = "Ошибка: Файл не был загружен";
+                $res = "Файл не был загружен";
             }
         } else {
             $res = "Ошибка передачи файла на сервер";
@@ -53,12 +55,12 @@ class Form
         return $res;
     }
 
-    function delete_file(): string
+    public function delete_file(): string
     {
         $res = false;
-        $file = self::SAVE_DIR . basename($this->file['name']);
+        $file = self::SAVE_DIR . $this->get_file_name();
 
-        if (isset($file)) {
+        if (file_exists($file)) {
             if (unlink($file)) {
                 $res = "Файл был успешно удален";
             } else {
@@ -70,11 +72,35 @@ class Form
 
         return $res;
     }
+
+    public function get_file_name(): string 
+    {
+        $res = basename($this->file['name']);
+
+        if(strlen($res) == 0) {
+            $res = 'Не удалось определить имя загруженного файла';
+        }
+
+        return $res;
+    }
+
+    public function check_form() {
+        $res = '';
+        // var_dump(get_object_vars($this));
+        foreach(get_object_vars($this) as $key => $option) {
+            if( empty($option) or is_null($option) ) {
+                $res .= "Поле формы \"$key\" не было задано<br>";
+            }
+        }
+        return $res;
+    }
 }
 
 $form = new Form();
 
 var_dump($form);
 
-echo $form->save_file();
-// echo $form->delete_file();
+echo $form->save_file() . "<br>";
+echo $form->delete_file() . "<br>";;
+echo $form->get_file_name() . "<br>";;
+echo $form->check_form();
